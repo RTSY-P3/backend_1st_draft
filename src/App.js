ec79d3be9fced96dc4118a209844e08b664768d7
Steven DeLitta <sdelitta@gmail.com>
2022-04-15 11:28:51 -0400

    Created CommentController.js and CommentRouter.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ const logger = require('morgan')
 const bodyParser = require('body-parser')
 const AuthRouter = require('./routes/AuthRouter')
 const PostRouter = require('./routes/PostRouter')
+const CommentRouter = require('./routes/CommentRouter')
 
 const app = express()
 
@@ -15,6 +16,8 @@ app.use(express.json())
 
 app.use('/auth', AuthRouter)
 app.use('/posts', PostRouter)
+app.use('/comments', CommentRouter)
+
 
 app.listen(PORT, () => console.log(`Server Running On Port: ${PORT}`))
 
